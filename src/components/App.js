0e5a3d3efd3441a6cd2d63b2113f700260853a38
painter98 <emailmepallavi@gmail.com>
2023-05-24 14:40:50 +0530

    change 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  let [component,usecomponent]=useState('');
+  usecomponent = 'I am learning React. My life is getting better.';
+  
   return (
-    <p>I am learning React. My life is getting better.</p>
+    <p>{component}</p>
   )
 }
 
