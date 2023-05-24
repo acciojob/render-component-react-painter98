8a904350292b44e82b32930252b0d40d1ddf52db
painter98 <emailmepallavi@gmail.com>
2023-05-24 14:45:32 +0530

    change 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
-  let [component,usecomponent]=useState('');
-  usecomponent = 'I am learning React. My life is getting better.';
-  
-  return (
-    <p>{component}</p>
-  )
+class App extends Component {
+  render(){
+    return (
+      <p>'I am learning React. My life is getting better.'</p>
+    )
+}
 }
 
 
